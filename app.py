8c4b3937cfd8bfbe4f522b265712bad4b0b3b460
Avinash Kumar <avinashrkmv@gmail.com>
2019-10-23 20:22:43 +0530

    added home page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask,render_template,url_for,request,redirect
 
 
+@app.route('/')
+def home():
+	return render_template('index.html')
+@app.route(/upload)
+	return render_template('upload.html')
 app = Flask(__name__)
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)

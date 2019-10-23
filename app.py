cf46463f9f71763dbac7385d9cafd7d9016096b8
Kinshuk Gupta <kinshukg31@gmail.com>
2019-10-23 22:19:41 +0530

    resolving error

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template,url_for,request,redirect
 
-
+app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template('index.html')
-@app.route(/upload)
-	return render_template('upload.html')
-app = Flask(__name__)
+@app.route('/upload')
+def upload():
+	return render_template('pages/upload.html')
+
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=True)
+	app.run(debug=True)
